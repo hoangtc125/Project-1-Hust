a11378ab4c191a7f2758682b4ff88b99935a7c78
hoangtc125 <anhhoangday.69@gmail.com>
2021-11-23 15:42:30 +0700

    change domain name

---
+++ src/main/java/com/prj1/services/CustomerService.java
@@ -0,0 +1,41 @@
+package com.prj1.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.prj1.dao.CustomerDAO;
+import com.prj1.entities.Customer;
+
+@Service
+@Transactional
+public class CustomerService {
+
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	public List<Customer> findAll() {
+		return customerDAO.findAll();
+	}
+
+	public Customer findById(int id) {
+		return customerDAO.findById(id);
+	}
+	
+	public void save(Customer customer){
+		// validate business
+		customerDAO.save(customer);
+	}
+
+	public void update(Customer customer){
+		// validate business
+		customerDAO.update(customer);
+	}
+	
+	public void delete(int id){
+		// validate business
+		customerDAO.delete(id);
+	}
+}
