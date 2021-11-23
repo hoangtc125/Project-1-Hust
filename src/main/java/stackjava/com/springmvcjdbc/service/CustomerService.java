f38c5553f89f7f770002254a563c615065070bf7
hoangtc125 <anhhoangday.69@gmail.com>
2021-11-23 11:38:57 +0700

    first commit

---
+++ src/main/java/stackjava/com/springmvcjdbc/service/CustomerService.java
@@ -0,0 +1,41 @@
+package stackjava.com.springmvcjdbc.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import stackjava.com.springmvcjdbc.dao.CustomerDAO;
+import stackjava.com.springmvcjdbc.entities.Customer;
+
+@Service
+@Transactional
+public class CustomerService {
+
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	public List<Customer> findAll() {
+		return customerDAO.findAll();
+	}
+
+	public Customer findById(int id) {
+		return customerDAO.findById(id);
+	}
+	
+	public void save(Customer customer){
+		// validate business
+		customerDAO.save(customer);
+	}
+
+	public void update(Customer customer){
+		// validate business
+		customerDAO.update(customer);
+	}
+	
+	public void delete(int id){
+		// validate business
+		customerDAO.delete(id);
+	}
+}
